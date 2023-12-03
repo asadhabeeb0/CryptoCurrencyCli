4e53f7602af80f4cabbaa852284e2311b3057a6b
asadhabeeb0 <asadhabeeb0@gmail.com>
2023-12-03 04:43:15 -0800

    Initial commit

---
+++ bin/coindex-key.js
@@ -0,0 +1,23 @@
+const program = require("commander");
+const key = require("../commands/key");
+
+program
+  .command("set")
+  .description("Set API Key -- Get at https://coinmarketcap.com")
+  .action(key.set);
+
+program
+  .command("show")
+  .description("Show API Key")
+  .action(key.show);
+
+program
+  .command("remove")
+  .description("Remove API Key")
+  .action(key.remove);
+
+program.parse(process.argv);
+
+if (!process.argv[2]) {
+  program.outputHelp();
+}
---
+++ utils/validation.js
@@ -0,0 +1,3 @@
+// Required Fields
+const isRequired = (input) => (input === "" ? "This value is required" : true);
+module.exports = { isRequired };
